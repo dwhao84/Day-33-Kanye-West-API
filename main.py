4095dae8c413d0db026fedd6ed12508128301e0a
Dawei Hao <dwsamurai84@gmail.com>
2024-11-10 21:27:37 +0800

    upload content for main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from tkinter import *
-
+import requests
+from api import api_url
 
 def get_quote():
+    response = requests.get(api_url)
+    text = response.json()["quote"]
+    print(text)
+    canvas.itemconfig(quote_text, text=text) # 替換quote_text的內容，調整成text。
     pass
     #Write your code here.
-
 
 
 window = Tk()
@@ -14,13 +18,11 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes Here", width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
-
-
 
 window.mainloop()
